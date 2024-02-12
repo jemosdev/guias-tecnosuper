2ef3749827e8e674e797eb24408bd9a49afb526a
jemosdev <jerson.mosquera.p@gmail.com>
2024-02-12 00:43:29 -0500

    responsive design in header component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,10 +12,10 @@ function AboutUs() {
                     <h1 className="About-title text-light pt-5 pb-4">Sobre <b>Nosotros</b></h1>
                 </Row>
                 <Row>
-                    <Col md={8}>
+                    <Col md={7}>
                         <p className="about-text text-light rounded-4">Somos TecnoSuper una marca de distribución de tecnología para todo Colombia. Hacemos que la tecnología sea accesible para todos. No importa si estás reportado en sistemas de crédito, creemos que todos merecen la oportunidad de disfrutar de las últimas tendencias tecnológicas. Es por eso que ofrecemos opciones de crédito flexibles y con una excelente cuota inicial donde damos prioridad a tus necesidades y posibilidades, permitiéndote comenzar tu viaje tecnológico con facilidad.</p>
                     </Col>
-                    <Col md={4}>
+                    <Col md={5}>
                         <div className="about-image">
                             <div className="imageContainer">
                                 <img src={about_image} alt="about-image" className="about-image-img" />
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
         <div className="Hero-container">
             <Container>
                 <Row>
-                    <Col md={6}>
+                    <Col md={6} className="column-hero-1">
                         <div className="hero_left pt-4 pb-2">
                             <h1>Bienvenido a nuestra web de <span>guias para la app de tecnosuper</span></h1>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dolorem, architecto vitae culpa fugiat saepe voluptatibus ea dicta ipsum enim! Dolorum nulla incidunt sapiente ex harum. Maxime explicabo repudiandae sequi?</p>
@@ -20,7 +20,7 @@ function Hero() {
                             </button>
                         </div>
                     </Col>
-                    <Col md={6}>
+                    <Col md={6} className="column-hero-2">
                         <div className="hero_right">
                             <div className="imageContainer">
                                 <img src={hero_right} alt="hero-image" className="hero_right-img" />
