7c465916b6000d368b36c1f493499c9dca0878e7
jemosdev <jerson.mosquera.p@gmail.com>
2024-02-28 22:53:22 -0500

    improving contactus section with viewport height

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,7 @@ function AboutUs() {
                     <h1 className="About-title text-light pt-5 pb-4">Sobre <b>Nosotros</b></h1>
                 </Row>
                 <Row className="position-relative">
-                    <div className="about-text col-lg-9 col-md-10 col-sm-10 text-light rounded-4 d-flex">
+                    <div className="about-text col-lg-9 col-md-10 col-sm-10 text-light rounded-4">
                         <p>Somos TecnoSuper una marca de distribución de tecnología para todo Colombia. Hacemos que la tecnología sea accesible para todos. No importa si estás reportado en sistemas de crédito, creemos que todos merecen la oportunidad de disfrutar de las últimas tendencias tecnológicas. Es por eso que ofrecemos opciones de crédito flexibles y con una excelente cuota inicial donde damos prioridad a tus necesidades y posibilidades, permitiéndote comenzar tu viaje tecnológico con facilidad.</p>
                     </div>
                     <div className="about-image">
